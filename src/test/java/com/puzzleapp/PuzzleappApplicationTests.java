a6721d8ee8c743684177348f912ab7c463e3bebf
Maria Chirnogeanu <maria.chirnogeanu@stud.acs.upb.ro>
2025-12-03 15:50:49 +0200

    Project added

---
+++ src/test/java/com/puzzleapp/PuzzleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puzzleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
